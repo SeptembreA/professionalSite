2024df6d10333f52e2f1ec38936f7776426cfa30
SeptembreA <septembre.anderson@gmail.com>
2021-03-09 14:30:40 -0500

    EOD commit

---
+++ scripts/script.js
@@ -0,0 +1,12 @@
+// Scroll to top
+$(document).ready(function () {
+  $(window).scroll(function() {
+    if ($(this).scrollTop() > 50) {
+      $('.scrolltop:hidden').stop(true, true).fadeIn();
+    } else {
+      $('.scrolltop').stop(true, true).fadeOut();
+    }
+  });
+  
+  $(function () { $(".scroll").click(function () {$("html,body").animate({ scrollTop: $("#home").offset().top }, "1000"); return false }) })
+});
