a29e09f9fc4535908218a338e3f0946202743f19
SeptembreA <septembre.anderson@gmail.com>
2021-03-10 13:40:37 -0500

    Added smooth scroll and scroll to the top via JS

--- scripts/script.js
+++ scripts/script.js
@@ -1,12 +1,36 @@
 // Scroll to top
-$(document).ready(function () {
-  $(window).scroll(function() {
-    if ($(this).scrollTop() > 50) {
-      $('.scrolltop:hidden').stop(true, true).fadeIn();
-    } else {
-      $('.scrolltop').stop(true, true).fadeOut();
+$(window).scroll(function() {
+  const height = $(window).scrollTop();
+  if (height > 100) {
+    $('#scrollToTop').fadeIn();
+  } else {
+    $('#scrollToTop').fadeOut();
+  }
+});
+
+$(document).ready(function() {
+  // Scroll to top
+  $('#scrollToTop').click(function(e) {
+    e.preventDefault();
+    $('html, body').animate({ scrollTop: 0 }, 'slow');
+    return false;
+  });
+
+  // Smooth scroll
+  $('a').on('click', function(e) {
+    // Make sure this.hash has a value before overriding default behavior
+    if (this.hash !== "") {
+      e.preventDefault();
+      // eslint-disable-next-line prefer-destructuring
+      const hash = this.hash;
+      $('html, body').animate({
+          scrollTop: $(hash).offset().top,
+        },
+        800,
+        function() {
+          window.location.hash = hash;
+        }
+      );
     }
   });
-  
-  $(function () { $(".scroll").click(function () {$("html,body").animate({ scrollTop: $("#home").offset().top }, "1000"); return false }) })
 });
